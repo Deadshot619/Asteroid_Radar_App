a2b4643e74c046850e9d2c4fe6a5d8e3d1750bc8
Pankaj Makhija <pankaj.makhija@bank-abc.com>
2021-06-02 13:31:05 +0530

    Show Asteroid Hazardous/Safe

--- starter/app/src/main/java/com/udacity/asteroidradar/utils/CustomBindingAdapter.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/utils/CustomBindingAdapter.kt
@@ -3,6 +3,7 @@ package com.udacity.asteroidradar.utils
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
@@ -20,6 +21,12 @@ fun bindRvAsteroidData(recyclerView: RecyclerView, data: List<Asteroid>?){
 fun bindIvAsteroidImage(imageView: ImageView, url: String?){
     Picasso.with(imageView.context).load(url).error(R.drawable.wallpapers).into(imageView)
 }
+
+@BindingAdapter("ivAsteroidHazardousIndicator")
+fun bindIvAsteroidHazardousIndicator(imageView: ImageView, isHazardous: Boolean?) {
+    imageView.setImageDrawable(ContextCompat.getDrawable(imageView.context, if (isHazardous == true) R.drawable.ic_status_potentially_hazardous else R.drawable.ic_status_normal))
+}
+
 
 @BindingAdapter("loadingStatus")
 fun bindLoadingStatus(progressBar: ProgressBar, status: Status?) {
