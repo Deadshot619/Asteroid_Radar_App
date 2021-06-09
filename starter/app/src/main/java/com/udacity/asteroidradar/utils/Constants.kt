c86e10b98f3368e95b33a958d54a14a524586ea3
Pankaj Makhija <pankaj.makhija@bank-abc.com>
2021-06-10 01:50:38 +0530

    - Refactor api key in build.gradle

--- starter/app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -1,13 +1,15 @@
 package com.udacity.asteroidradar.utils
 
+import com.udacity.asteroidradar.BuildConfig
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
 
 //    API Key
-    //TODO : Put your Api Key here
-    const val API_KEY = ""
+    //TODO : Put your Api Key in build.gradle
+    val API_KEY = BuildConfig.NASA_API_KEY
 
     //URL Path
     const val FEED = "neo/rest/v1/feed"
